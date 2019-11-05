a9740c1b5851b2c186b451130b8a132bccfa86e1
ruslanzharkov <zharkov.ruslan.e@gmail.com>
2019-11-05 23:59:29 +0300

    Removed .idea added game model

---
+++ src/models/game.ts
@@ -0,0 +1,14 @@
+import {Document, Schema} from 'mongoose';
+import mongoose from '../connections/mongo';
+
+const gameSchema = new Schema({
+    amount: {type: Number, required: true},
+    createdByCompany: {type: Number, required: true},
+    createdDate: {type: String, required: true},
+    description: {type: String, required: true},
+    name: {type: String, required: true, unique: true},
+    postedBy: {type: String, required: true, unique: true},
+    price: {type: Number, required: true},
+    thumbnailPhoto: {type: String, required: true},
+    updatedDate: {type: String, required: true},
+});
