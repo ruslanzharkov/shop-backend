6fd0f729d57d6fd6dcfb257e20ed891ab85b07d1
ruslanzharkov <zharkov.ruslan.e@gmail.com>
2019-11-06 00:44:35 +0300

    Refactored game route file

--- src/routes/game.ts
+++ src/routes/game.ts
@@ -2,11 +2,9 @@ import express, { Router } from 'express';
 import GamesController from '../controllers/Games.controller';
 
 const router: Router = express.Router();
-
 const gamesController = new GamesController();
 
 router.get('/games', gamesController.getGames.bind(this));
-
 router.post('/games', gamesController.createGame.bind(this));
 
 export default router;
